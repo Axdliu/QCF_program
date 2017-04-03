42fec476c6fdfa1cd6e1c6e83d305e4b047f6fe5
Axdliu <axdliu@gmail.com>
2017-04-02 23:55:30 -0400

    using logic regression method

--- risk_management/Credit_risk_analysis.R
+++ risk_management/Credit_risk_analysis.R
@@ -131,4 +131,12 @@ predictions_all_full <- predict(log_model_full, newdata =test_set, type = "respo
 # Look at the predictions range
 range(predictions_all_full)
 
+# The code for the logistic regression model and the predictions is given below
+log_model_full <- glm(loan_status ~ ., family = "binomial", data = training_set)
+predictions_all_full <- predict(log_model_full, newdata = test_set, type = "response")
 
+# Make a binary predictions-vector using a cut-off of 15%
+pred_cutoff_15 = ifelse(predictions_all_full > 0.15, 1, 0)
+
+# Construct a confusion matrix
+table(test_set$loan_status, pred_cutoff_15)
